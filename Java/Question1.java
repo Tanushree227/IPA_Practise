d8aa76dffe909700c927f87595488b846c34618e
Tanushree227 <tanushreedas813@gmail.com>
2024-10-13 16:29:21 +0530

    question 1

---
+++ Java/Question1.java
@@ -0,0 +1,4 @@
+/*
+ * Write a main method in Solution class, In the main method, read a string value and print the count of lower class characters present in the input String value.
+ * If no lower class characters are present in the String value then it should print "No lower case characters present" as a string.
+ */
